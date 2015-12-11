33b297d81b413c9776f83ca100a8fe2850d0a624
wesley <wesley@wd-media.nl>
2015-12-11 14:25:02 -0500

    Working LAMP configuration

--- sites/host1.com/index.php
+++ sites/host1.com/index.php
@@ -1,1 +1,19 @@
-host 1 .com
+<?php
+function connect(){
+    //$db = \Rhonda\Config::get('DB')->connections->local;
+    //$db = DB::$db;
+    $mysqli = new mysqli('192.168.99.100',
+      'root',
+      'MY_PASSWORD',
+      'YOUR_DATABASE_NAME',
+      '3306');
+    $mysqli->set_charset("utf8");
+
+    //DB::$mysqli = $mysqli;
+    return $mysqli;
+  }
+
+$mysqli = connect();
+
+
+  ?>
--- sites/host1.com/info.php
+++ sites/host1.com/info.php
@@ -1,3 +1,4 @@
 <?php
-phpinfo();
+throw new Exception("Error Processing Request");
+
 ?>
